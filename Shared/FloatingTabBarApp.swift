822ad44d69a2270ef843b6695b838d543064fe1a
mingyukim <gyuinsky@gmail.com>
2021-08-16 13:46:14 +0900

    Initial Commit

---
+++ Shared/FloatingTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingTabBarApp.swift
+//  Shared
+//
+//  Created by MIN on 2021/08/16.
+//
+
+import SwiftUI
+
+@main
+struct FloatingTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
